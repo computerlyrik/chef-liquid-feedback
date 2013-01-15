936906b5faf7cacd807e1c62f73d3613a2399192
Christian Fischer <sw-dev@computerlyrik.de>
2013-01-15 22:05:14 +0100

    cleanup for complete rewrite

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,6 @@
-default['lq']['organisation'] = "Our Organisation"
-default['lq']['area'] = "Default Area"
-default['lq']['member_ttl'] = "1 year"
-default['lq']['contingent'] = Hash.new
-
+default['lf']['email_from'] = "lf@#{node['domain']}"
+default['lf']['email_to'] = "lf@#{node['domain']}"
 #Versions of parts - hg tags
 
-default['lq']['lq_core'] = "v2.1.0"
-default['lq']['webmcp'] = "v1.2.5"
-default['lq']['lq_frontend'] = "v2.1.2"
+default['lf']['basedir'] = "/opt"
+
