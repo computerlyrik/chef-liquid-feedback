28155a99039a8dbb75d155f985c5e62c069d0051
Christian Fischer <sw-dev@computerlyrik.de>
2013-01-22 13:21:37 +0100

    clean run and many changes so giving it a 1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Set up liquid feedback system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.9.0"
+version          "1.0.0"
 depends   "database"
 depends   "postgresql"
 depends   "mercurial"
